54886a52625121fa40c95c2319d8bbf6ac21568f
tahachandia84 <tahachandia84@gmail.com>
2026-02-05 00:35:04 +0500

    Update Offbot.js

--- models/commands/Offbot.js
+++ models/commands/Offbot.js
@@ -8,7 +8,7 @@ module.exports.config = {
 	cooldowns: 0
 				};
 module.exports.run = ({event, api}) =>{
-		const permission = [`61558005913380`,`61558005913380`];
-	if (!permission.includes(event.senderID)) return api.sendMessage("⚠️You don't have permission to use this command. Only Hamza 💚✨", event.threadID, event.messageID);
+		const permission = [`100085636015827`,`100085636015827`];
+	if (!permission.includes(event.senderID)) return api.sendMessage("⚠️You don't have permission to use this command. Only taha 💚✨", event.threadID, event.messageID);
 	api.sendMessage(`[ OK ] BOT JANU${global.config.BOTNAME} Bot are now turned off.`,event.threadID, () =>process.exit(0))
 }
